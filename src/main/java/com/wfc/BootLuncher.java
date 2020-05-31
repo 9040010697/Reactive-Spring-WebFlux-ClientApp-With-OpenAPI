662dd8d7a077fb59788e67941220797ec3729ee0
9040010697 <52064780+9040010697@users.noreply.github.com>
2020-05-31 15:26:34 +0530

    Add files via upload

---
+++ src/main/java/com/wfc/BootLuncher.java
@@ -0,0 +1,14 @@
+package com.wfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLuncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLuncher.class, args);
+	}
+	
+
+}
